71b9c4e575c0b73d1ee6dd117514fc0bc68ab72d
Jesús Zelaya <88550504+jesuszelayac@users.noreply.github.com>
2024-03-12 05:56:06 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,38 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // This function will be executed after the window is loaded
+  function generateExcuse() {
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    let whoIndex = Math.floor(Math.random() * who.length);
+    let actionIndex = Math.floor(Math.random() * action.length);
+    let whatIndex = Math.floor(Math.random() * what.length);
+    let whenIndex = Math.floor(Math.random() * when.length);
+
+    let excuse =
+      who[whoIndex] +
+      " " +
+      action[actionIndex] +
+      " " +
+      what[whatIndex] +
+      " " +
+      when[whenIndex];
+    document.getElementById("excuse").innerHTML = excuse;
+  }
+
+  // Generate the first excuse when the page loads
+  generateExcuse();
+
+  // Attach the generateExcuse function to the button click event
+  document.querySelector("button").onclick = generateExcuse;
 };
